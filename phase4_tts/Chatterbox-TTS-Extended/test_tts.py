05a860d9e10cc1dac0106273e159b0a19ab7990d
Paul Burton <pauldavidburton@gmail.com>
2025-11-10 22:55:36 -0600

    kokoro 1

--- phase4_tts/Chatterbox-TTS-Extended/test_tts.py
+++
@@ -1,7 +0,0 @@
-import torch
-from chatterbox.src.chatterbox.tts import ChatterboxTTS
-model = ChatterboxTTS.from_pretrained(device='cpu')
-wav = model.generate('Test sentence for TTS.', language_id='en')
-import torchaudio
-torchaudio.save('test.wav', wav, 24000)
-print('Test WAV generated.')
